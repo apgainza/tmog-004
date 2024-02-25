b75ced85714eccc14ffc16cbf816f7fc926e3495
Alejandro Portilla Gainza <aportiga@mercadona.es>
2024-02-25 15:34:52 +0100

    feat: Merge list

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,37 @@
 package org.tfoc;
 
+import java.util.Objects;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public ListNode mergeTwoLists(
+    public static ListNode mergeTwoLists(
             ListNode list1,
             ListNode list2) {
 
-        return null;
-    }
+        ListNode response = new ListNode();
+        ListNode currentNode = response;
 
+        while (true) {
+            if (Objects.isNull(list1) || Objects.isNull(list1.val)) {
+                currentNode.next = list2;
+                break;
+            } else if (Objects.isNull(list2) || Objects.isNull(list2.val)) {
+                currentNode.next = list1;
+                break;
+            } else {
+                if (list1.val >= list2.val) {
+                    currentNode.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                } else {
+                    currentNode.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                }
+                currentNode = currentNode.next;
+            }
+        }
+        return response.next;
+    }
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,38 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Objects;
+import java.util.stream.Stream;
 
 @Slf4j
 class SolutionTest {
 
-    @Test
-    void testSolution() {
+    private static Stream<Arguments> provideListNodes() {
+        return Stream.of(
+                Arguments.of(new ListNode(1, new ListNode(2)), new ListNode(1), "112"),
+                Arguments.of(new ListNode(1, new ListNode(2, new ListNode(4))),
+                        new ListNode(1, new ListNode(3, new ListNode(4))), "112344"),
+                Arguments.of(null, new ListNode(0), "0")
+        );
+    }
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+    @ParameterizedTest
+    @MethodSource("provideListNodes")
+    void testSolution1(ListNode l1, ListNode l2, String responseExpected) {
+        StringBuilder response = new StringBuilder();
+        ListNode listNode = Solution.mergeTwoLists(l1, l2);
+
+        while (Objects.nonNull(listNode)) {
+            response.append(listNode.val);
+            listNode = listNode.next;
+        }
+
+        Assertions.assertEquals(response.toString(), responseExpected);
     }
 
 }
